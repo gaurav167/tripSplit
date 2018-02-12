262f9b3e6d1ee4c081ba6d6b77a7d69de8029252
Harsh Bhardwaj <harshbhardwaj2357@gmail.com>
2018-02-13 04:59:57 +0530

    Added icons and fixed bugs

--- Android/app/src/main/java/me/kungfucat/tripsplit/HistoryFragment.java
+++ Android/app/src/main/java/me/kungfucat/tripsplit/HistoryFragment.java
@@ -1,12 +1,15 @@
 package me.kungfucat.tripsplit;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -36,17 +39,56 @@ public class HistoryFragment extends Fragment {
         int userId = bundle.getInt("userId");
 
         ArrayList<HistoryModel> historyModels = getTransactions(userId);
-        RecyclerView recyclerView = row.findViewById(R.id.historyRecyclerView);
-        HistoryRecyclerViewAdapter historyRecyclerViewAdapter = new HistoryRecyclerViewAdapter(getContext(), historyModels);
-        recyclerView.setAdapter(historyRecyclerViewAdapter);
+        ListView listView = row.findViewById(R.id.historyListView);
+        HistoryAdapter adapter = new HistoryAdapter(getContext(), historyModels);
+        listView.setAdapter(adapter);
         return row;
+    }
+
+    class HistoryAdapter extends BaseAdapter {
+        ArrayList<HistoryModel> arrayList;
+        Context context;
+
+        public HistoryAdapter(Context context, ArrayList<HistoryModel> arrayList) {
+            this.context = context;
+            this.arrayList = arrayList;
+        }
+
+        @Override
+        public int getCount() {
+            return arrayList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return i;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View viewRow = getLayoutInflater().inflate(R.layout.history_custom_row, null);
+            TextView transaction = viewRow.findViewById(R.id.transactionAmount);
+            TextView timeStamp = viewRow.findViewById(R.id.timeStamp);
+            TextView groupNameTextView = viewRow.findViewById(R.id.groupNameTextView);
+
+            transaction.setText(arrayList.get(i).getValue()+"");
+            timeStamp.setText(arrayList.get(i).getTimeStamp()+"");
+            groupNameTextView.setText(arrayList.get(i).getGroupId() + " ABCD");
+            return viewRow;
+        }
     }
 
     public ArrayList<HistoryModel> getTransactions(int userId) {
         ArrayList<HistoryModel> historyModels = new ArrayList<>();
         HistoryModel historyModel1 = new HistoryModel(),
                 historyModel2 = new HistoryModel(),
-                historyModel3 = new HistoryModel();
+                historyModel3 = new HistoryModel(),
+                historyModel4 = new HistoryModel();
 
         historyModel1.setTimeStamp("02/13/18");
         historyModel1.setTransactionId(102);
@@ -73,13 +115,13 @@ public class HistoryFragment extends Fragment {
 
         historyModels.add(historyModel3);
 
-        historyModel2.setTimeStamp("05/13/18");
-        historyModel2.setTransactionId(105);
-        historyModel2.setGroupId(3);
-        historyModel2.setUserId(1);
-        historyModel2.setValue(12);
+        historyModel4.setTimeStamp("05/13/18");
+        historyModel4.setTransactionId(105);
+        historyModel4.setGroupId(3);
+        historyModel4.setUserId(1);
+        historyModel4.setValue(12);
 
-        historyModels.add(historyModel3);
+        historyModels.add(historyModel4);
 
         return historyModels;
     }
