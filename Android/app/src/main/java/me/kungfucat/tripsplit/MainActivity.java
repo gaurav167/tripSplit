9656d5beac29593395bc444cdf50fe1eaa9b33b6
ishita sharma <ishitasharma12699@gmail.com>
2018-02-13 06:55:30 +0530

    Adding profile for user and linking up activities for adding bill and groups.

--- Android/app/src/main/java/me/kungfucat/tripsplit/MainActivity.java
+++ Android/app/src/main/java/me/kungfucat/tripsplit/MainActivity.java
@@ -1,5 +1,6 @@
 package me.kungfucat.tripsplit;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -7,6 +8,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -84,6 +87,35 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.make_a_group_action) {
+           Intent intent = new Intent(MainActivity.this,MakeAGroupActivity.class);
+           startActivity(intent);
+            return true;
+        }
+        else if(id== R.id.profile_activity_action){
+            Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+            startActivity(intent);
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void setUpPieChart() {
